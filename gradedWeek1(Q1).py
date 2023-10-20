107f664aff2519f8487d51e075050964991d6831
prof "F.M.P <107954137+Fatimamehmud@users.noreply.github.com>
2023-10-21 02:23:14 +0300

    Update gradedWeek1(Q1).py

--- gradedWeek1(Q1).py
+++ gradedWeek1(Q1).py
@@ -1,3 +1,17 @@
+
+#this is the content of the file school_prompt2.txt :
+#Writing essays for school can be difficult but
+#many students find that by researching their topic that they
+#have more to say and are better informed. Here are the university
+#we require many undergraduate students to take a first year writing requirement
+#so that they can
+#have a solid foundation for their writing skills. This comes
+#in handy for many students.
+#Different schools have different requirements, but everyone uses
+#writing at some point in their academic career, be it essays, research papers,
+#technical write ups, or scripts.
+
+
 # Using the file school_prompt2.txt, find the number of characters in the file and assign that value to the variable num_char.
 
 myfile=open("school_prompt2.txt","r")
