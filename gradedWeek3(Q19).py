7af82c09fd5eaa6f6a0384608806d5302113608a
prof "F.M.P <107954137+Fatimamehmud@users.noreply.github.com>
2023-11-04 23:02:26 +0300

    Create gradedWeek3(Q19).py

---
+++ gradedWeek3(Q19).py
@@ -0,0 +1,9 @@
+#Create a tuple called practice that has four elements: ‘y’, ‘h’, ‘z’, and ‘x’.
+practice=()
+x=list(practice)
+x.append('y')
+x.append('h')
+x.append('z')
+x.append('x')
+practice=tuple(x)
+print(practice)
